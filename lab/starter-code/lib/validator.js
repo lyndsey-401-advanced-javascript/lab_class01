354e3f40c82a584a210eea23245ce02af77d3fe9
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-23 14:00:20 -0700

    eslint fixes

--- lab/starter-code/lib/validator.js
+++ lab/starter-code/lib/validator.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Vinicio - this is similar to module.exports = {};, but you are giving it an easier to use name
-let validator = module.exports = {};
 //allows for export to test outside of file
+let validator = module.exports = {};
 
 validator.isLessThanTen = () => {};
 
@@ -12,38 +12,15 @@ validator.isObjectValid = (data, schema) => {
   //what assumptions do we have so far? 
   //data is going to be an object
   //schema will be an object that has a property called fields 
-  //TODO: validate our invariants 
+
   Object.keys(schema.fields).forEach(expectedProperty => {
     if (!data.hasOwnProperty(expectedProperty)){
       return false;
     }
     console.log(data[expectedProperty]); //value
+    //is the value correct
     console.log(schema.fields[expectedProperty].type); //type
     //is the value correct based on the type 
   });
   return true;
 };
-//TODO: validate types
-
-
-
-// /**
-//  * Based on a set of rules, is the input valid?
-//  * TODO: Define the rules ... how do we send them in? How do we identify?
-//  * @param input
-//  * @param rules
-//  * @returns {boolean}
-//  */
-// validator.isValid = (input, rules) => {
-//   return true;
-// };
-
-// /**
-//  * Is this a string?
-//  * @param input
-//  * @returns {boolean}
-//  */
-
-// validator.isString = (input) => {
-//   return typeof input === 'string';
-// };
---
+++ validator.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const validator = require('./lab/starter-code/lib/validator.js');
+
+
+describe('#validator', () => {
+  describe('#isLessThanTen', () => {
+    test('regular cases', () => {
+      expect(validator.isLessThanTen(5)).toEqual(true);
+    });
+  });
+});
+
+describe('#isObjectValid', () => {
+  test ('regular cases', () => {
+    const schema = {
+      fields: {
+        id: {type: 'string'}, //if there is an id it HAS to be a string
+        age: {type: 'number'}, //if there is an age it HAS to be a number etc 
+        favoriteToys: {type: 'object'},
+      },
+    };
+    expect(validator.isObjectValid({id: 'kali', age: 2, favoriteToys: {}}, schema));
+  }).toEqual(true);
+});
+
